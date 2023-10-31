3f9bd3a5614704402ff4011b3bdba8eb523fab3b
rrachea <rrachelsng@gmail.com>
2023-10-31 19:48:15 +0800

    adding dialogue c# scripts

---
+++ Assets/Scripts/Dialogue/TriggerDialogue.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TriggerDialogue : MonoBehaviour
+{
+    [SerializeField] private Dialogue _dialogue = null;
+
+    private const string UI_TRIGGER_TAG = "UITrigger";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(UI_TRIGGER_TAG))
+        {
+            Debug.Log("in contact w UI trigger object");
+            DialogueText dialogueText = other.GetComponent<DialogueText>();
+            if (dialogueText == null) Debug.LogError("No DialogueText found for UITrigger tagged collider. Possibly you forgot to add it!!!");
+            else
+            {
+                _dialogue.LoadDialogueLines(dialogueText.lines);
+                _dialogue.StartDialogue();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(UI_TRIGGER_TAG))
+        {
+            Debug.Log("left contact w UI trigger object");
+            _dialogue.EndDialogue();
+        }
+    }
+}
