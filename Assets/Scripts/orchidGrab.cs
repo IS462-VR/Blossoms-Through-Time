90dd99d93e3a918dcef0981bc14a96f2117ee602
tan-sd <shengdatan@gmail.com>
2023-11-03 15:49:28 +0800

    Add orchidGrab script

---
+++ Assets/Scripts/orchidGrab.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class orchidGrab : MonoBehaviour
+{
+    public void OnGrabbed()
+    {
+        // Load the scene
+        Debug.Log("Grabbed");
+        SceneManager.LoadScene("Scene 2");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
