181ba100b63ea73c06b3e056c1f79795e94d2994
rrachea <rrachelsng@gmail.com>
2023-11-06 01:04:49 +0800

    Revert "Merge branch 'main' into AddDialogue#2"
    This reverts commit 86031c225dba9bf49e81b162554145ef8d150539, reversing
    changes made to b9c3cd6af5fc2f81189109e790f23c5405151d44.

--- Assets/Scripts/Dialogue/TriggerDialogue.cs
+++ Assets/Scripts/Dialogue/TriggerDialogue.cs
@@ -5,20 +5,25 @@ public class TriggerDialogue : MonoBehaviour
     [SerializeField] private Dialogue _dialogue = null;
 
     private const string UI_TRIGGER_TAG = "UITrigger";
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag(UI_TRIGGER_TAG)) {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(UI_TRIGGER_TAG))
+        {
             Debug.Log("in contact w UI trigger object");
             DialogueText dialogueText = other.GetComponent<DialogueText>();
             if (dialogueText == null) Debug.LogError("No DialogueText found for UITrigger tagged collider. Possibly you forgot to add it!!!");
-            else {
+            else
+            {
                 _dialogue.LoadDialogueLines(dialogueText.lines);
                 _dialogue.StartDialogue();
             }
         }
     }
 
-    private void OnTriggerExit(Collider other) {
-        if (other.CompareTag(UI_TRIGGER_TAG)) {
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(UI_TRIGGER_TAG))
+        {
             Debug.Log("left contact w UI trigger object");
             _dialogue.EndDialogue();
         }
