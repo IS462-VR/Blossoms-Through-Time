d0c09ab6358824b3b9e21185c67cee508a9e78b7
Dylan Teoh Jin Sheng <jsteoh.2021@scis.smu.edu.sg>
2023-11-10 10:45:31 +0800

    Chaneges

--- Assets/ScoopingWaterAction.cs
+++ Assets/ScoopingWaterAction.cs
@@ -8,15 +8,19 @@ public class ScoopingWaterAction : MonoBehaviour
     //public GameObject cup;
     private GameObject water;
     public GameObject socket;
+    public GameObject obj;
+    private RecipeSteps recipeSteps;
 
     private void Start()
     {
         // Find the full and empty bowl child objects.
         //cup = rosellecup.Find("Empty Cup").gameObject;
         water = rosellecup.Find("Water for Cup").gameObject;
+        recipeSteps = obj.GetComponent<RecipeSteps>();
+
 
         // Initially, hide the water.
-        
+
         water.SetActive(false);
     }
 
@@ -25,6 +29,8 @@ public class ScoopingWaterAction : MonoBehaviour
         // Hide the full bowl and show the empty bowl and puddle.
         water.SetActive(true);
         socket.SetActive(true);
+        recipeSteps.NextStep();
+        Debug.Log("works");
     }
 
     private void OnTriggerEnter(Collider other)
