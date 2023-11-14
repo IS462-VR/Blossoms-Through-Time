92e79acab5a79c37bd69707368ea8ba47fb9ab11
Dylan Teoh Jin Sheng <jsteoh.2021@scis.smu.edu.sg>
2023-11-14 13:53:18 +0800

    New script edits

--- Assets/Scripts/TriggerScene4.cs
+++ Assets/Scripts/TriggerScene4.cs
@@ -10,19 +10,10 @@ public class TriggerScene4 : MonoBehaviour
     private const string UI_TRIGGER_TAG = "UITrigger";
     public AudioSource audioSource = null;
 
-    private void Awake()
+    private void Start()
     {
-        if (audioSource == null)
-        {
+        
             Debug.LogError("TriggerDialogue's audioSource for lineClips not assigned!");
-        }
-    }
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
-        
             //Debug.Log("OnTriggerEnter - UI trigger object");
             DialogueText dialogueText = obj.GetComponent<DialogueText>();
             //Vector3 colliderPosition = other.transform.position;
@@ -34,15 +25,34 @@ public class TriggerScene4 : MonoBehaviour
                 _dialogue.StartDialogue();
             }
         
-
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag(UI_TRIGGER_TAG))
-        {
-            Debug.Log("OnTriggerExit - UI trigger object");
-            _dialogue.EndDialogue();
-        }
     }
 }
+
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+        
+        
+//            //Debug.Log("OnTriggerEnter - UI trigger object");
+//            DialogueText dialogueText = obj.GetComponent<DialogueText>();
+//            //Vector3 colliderPosition = other.transform.position;
+//            //Debug.Log(colliderPosition);
+//            if (dialogueText == null) Debug.LogError("No DialogueText found for UITrigger tagged collider. Possibly you forgot to add it!!!");
+//            else
+//            {
+//                _dialogue.LoadDialogueData(dialogueText, audioSource);
+//                _dialogue.StartDialogue();
+//            }
+        
+
+//    }
+
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag(UI_TRIGGER_TAG))
+//        {
+//            Debug.Log("OnTriggerExit - UI trigger object");
+//            _dialogue.EndDialogue();
+//        }
+//    }
+//}
