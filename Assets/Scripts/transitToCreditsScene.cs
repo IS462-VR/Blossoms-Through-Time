cfac1e9a7e5f263845f1772a4e3e439baee85c67
tan-sd <shengdatan@gmail.com>
2023-11-16 01:39:32 +0800

    Add fade in effect

--- Assets/Scripts/transitToCreditsScene.cs
+++ Assets/Scripts/transitToCreditsScene.cs
@@ -2,23 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class transitToCreditsScene : MonoBehaviour
 {
-    public float delayBeforeLoad = 1.0f;
+    public float delayBeforeLoad = 25.0f;
+    public float delayForFade = 15.0f;
     public string sceneToLoad = "CreditScene";
+    public Image[] imagesToFade;
+    private float startTime;
+    private Color startColor;
+    private bool isFading = false;
 
     IEnumerator WaitAndLoadScene()
     {
         // Wait for the specified delay
         yield return new WaitForSeconds(delayBeforeLoad);
 
+        isFading = true;
+
+        startTime = Time.time;
+
+        while (isFading)
+        {
+            float t = (Time.time - startTime) / delayForFade;
+
+            if (t >= 3f)
+            {
+                isFading = false;
+                SceneManager.LoadScene(sceneToLoad);
+            }
+            else
+            {
+                Color newColor = startColor;
+                newColor.a = Mathf.Lerp(0f, 1f, t);
+
+                foreach (Image image in imagesToFade)
+                {
+                    image.color = newColor;
+                }
+                yield return null;
+            }
+        }
+
         // Load the new scene
-        SceneManager.LoadScene(sceneToLoad);
+        // SceneManager.LoadScene(sceneToLoad);
     }
     // Start is called before the first frame update
     void Start()
     {
+        // startTime = Time.time;
+        startColor = imagesToFade[0].color;
         StartCoroutine(WaitAndLoadScene());
     }
 
